72841be837d9528ebd201564f86da95459f1a707
olingomaxwell1999 <olingomaxwell1999@gmail.com>
2023-09-18 20:41:28 +0300

    introduction to operators

---
+++ app/js/operators.js
@@ -0,0 +1,3 @@
+//What are operations in javascript?- In JavaScript, operators are symbols that perform specific mathematical or logical operations on one or more operands. These operands can be variables, constants, or expressions. Operators are used to perform a variety of tasks, such as addition, subtraction, multiplication, division, and comparison.
+
+//
