25be99cb2daa5122903c27188af113db63fb254e
olingomaxwell1999 <olingomaxwell1999@gmail.com>
2023-09-20 12:11:10 +0300

    updated readme file

--- app/js/operators.js
+++ app/js/operators.js
@@ -1,3 +1,15 @@
 //What are operations in javascript?- In JavaScript, operators are symbols that perform specific mathematical or logical operations on one or more operands. These operands can be variables, constants, or expressions. Operators are used to perform a variety of tasks, such as addition, subtraction, multiplication, division, and comparison.
 
-//
+//There are several different types of operators in JavaScript, including:
+
+//Arithmetic operators: These operators perform mathematical operations on one or more operands. Examples include the addition (+) operator, the subtraction (-) operator, the multiplication (*) operator, and the division (/) operator.
+
+//Assignment operators: These operators assign a value to a variable. Examples include the assignment (=) operator, the addition assignment (+=) operator, and the subtraction assignment (-=) operator.
+
+//Comparison operators: These operators compare two operands and return a Boolean value indicating whether the comparison is true or false. Examples include the equality (==) operator, the inequality (!=) operator, the greater than (>) operator, and the less than (<) operator.
+
+//Logical operators: These operators perform logical operations on one or more operands. Examples include the logical AND (&&) operator, the logical OR (||) operator, and the logical NOT (!) operator.
+
+// Increment and decrement operators: These operators increment or decrement the value of a variable by one. Examples include the increment (++) operator and the decrement (--) operator.
+
+//Bitwise operators: These operators perform bitwise operations on one or more operands. Examples include the AND (&) operator, the OR (|) operator, and the XOR (^) operator.
---
+++ app/js/simplecomparisons.js
@@ -0,0 +1,3 @@
+//In this case, we are going to look at equality
+
+//we are going to meet signs like == === != !== < > 
